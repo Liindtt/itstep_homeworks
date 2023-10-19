08095ea811415f641664805d80f5c26b3058a199
Yaroslav <lindtt.10.2003@gmail.com>
2023-10-19 22:12:13 +0300

    adding homework 62

--- 62 lesson/test_vector.py
+++ 62 lesson/test_vector.py
@@ -22,10 +22,21 @@ def test_int(create_vector):
     obj_vector = create_vector
     assert int(Vector.length(obj_vector)) == int(sqrt((obj_vector.x ** 2 + obj_vector.y ** 2 + obj_vector.z ** 2)))
 
-def test_neg(create_vector):
-    obj_vector = create_vector
-    assert -obj_vector == Vector(obj_vector.x, obj_vector.y, obj_vector.z)
+# test the addition of two vectors
+def test_add(create_vector):
+    new_obj = create_vector + Vector(3, 6, 9)
+    assert create_vector != new_obj
+    assert new_obj == Vector(12, 12, 12)
 
+# test the multiple vector on number
+def test_mul(create_vector):
+    new_obj = create_vector * 5
+    assert new_obj == Vector(create_vector.x * 5, create_vector.y * 5, create_vector.z * 5)
+    assert new_obj == Vector(45, 30, 15)
 
-
-
+# test -= of two vectors
+def test_isub(create_vector):
+    vector1 = Vector(20, 10, 5)
+    vector2 = Vector(15, 5, 0)
+    vector1 -= vector2
+    assert vector1 == Vector(5, 5, 5)
